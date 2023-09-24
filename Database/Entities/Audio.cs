bace101c0bf6e11746f0952c6cd35ea19298c119
Evgeny Lyashenko <eugene.code@yandex.ru>
2023-09-24 17:09:17 +0300

    Add comments to the Audio class

--- Database/Entities/Audio.cs
+++ Database/Entities/Audio.cs
@@ -2,16 +2,40 @@
 
 namespace Database.Entities;
 
+/// <summary>
+/// Представляет таблицу с аудиофайлами.
+/// </summary>
+[Index(nameof(Source), IsUnique = true)]
+[Comment("Аудиофайлы.")]
 public class Audio
 {
+    /// <summary>
+    /// ID аудиофайла.
+    /// </summary>
     [ForeignKey(nameof(Translation))]
+    [Comment("ID аудиофайла.")]
     public int Id { get; set; }
 
+    /// <summary>
+    /// ID голоса, который озвучил этот аудиофайл.
+    /// </summary>
+    [Comment("ID голоса, который озвучил этот аудиофайл.")]
     public int VoiceId { get; set; }
 
+    /// <summary>
+    /// Ссылка на аудиофайл.
+    /// </summary>
     [MaxLength(256)]
+    [Comment("Ссылка на аудиофайл.")]
     public Uri? Source { get; set; }
 
+    /// <summary>
+    /// Перевод, для которого предназначен этот аудиофайл.
+    /// </summary>
     public Translation? Translation { get; set; }
+
+    /// <summary>
+    /// Голос, который озвучил этот аудиофайл.
+    /// </summary>
     public Voice? Voice { get; set; }
 }
