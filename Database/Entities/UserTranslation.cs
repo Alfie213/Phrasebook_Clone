ff499a56a9614a51cce0b6484576b8169e3b6f55
Evgeny Lyashenko <eugene.code@yandex.ru>
2023-09-24 18:31:55 +0300

    Update UserTranslation.cs

--- Database/Entities/UserTranslation.cs
+++ Database/Entities/UserTranslation.cs
@@ -2,13 +2,50 @@
 
 namespace Database.Entities;
 
-public class UserTranslation
+/// <summary>
+/// Таблица, описывающая взаимодействие пользователя с переводами.
+/// </summary>
+[Index(nameof(UserId), nameof(TranslationId), IsUnique = true)]
+[Comment("Взаимодействие пользователя с переводами.")]
+public sealed class UserTranslation
 {
+    /// <summary>
+    /// ID взаимодействия.
+    /// </summary>
+    [Comment("ID взаимодействия.")]
+    public int Id { get; set; }
+
+    /// <summary>
+    /// ID пользователя.
+    /// </summary>
+    [Comment("ID пользователя.")]
     public required string UserId { get; set; }
+
+    /// <summary>
+    /// ID перевода, с которым взаимодействует пользователь.
+    /// </summary>
+    [Comment("ID перевода, с которым взаимодействует пользователь.")]
     public int TranslationId { get; set; }
+
+    /// <summary>
+    /// Определяет, выучил ли пользователь переведённую фразу.
+    /// </summary>
+    [Comment("Определяет, выучил ли пользователь переведённую фразу.")]
     public bool Learned { get; set; }
+
+    /// <summary>
+    /// Определяет, скрыл ли пользователь переведённую фразу.
+    /// </summary>
+    [Comment("Определяет, скрыл ли пользователь переведённую фразу.")]
     public bool Hidden { get; set; }
 
-    public IdentityUser? User { get; set; }
-    public Translation? Translation { get; set; }
+    /// <summary>
+    /// Пользователь.
+    /// </summary>
+    public IdentityUser? User { get; }
+
+    /// <summary>
+    /// Перевод, с которым взаимодействует пользователь.
+    /// </summary>
+    public Translation? Translation { get; }
 }
