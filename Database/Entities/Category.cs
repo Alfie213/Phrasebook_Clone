60f8866ec04900997304aa188dbb9eca4f171d01
Evgeny Lyashenko <eugene.code@yandex.ru>
2023-09-24 18:02:40 +0300

    Add comments to the Category class

--- Database/Entities/Category.cs
+++ Database/Entities/Category.cs
@@ -1,15 +1,39 @@
 ﻿namespace Database.Entities;
 
+/// <summary>
+/// Представляет таблицу с категориями.
+/// </summary>
 [Index(nameof(Name), IsUnique = true)]
+[Comment("Категории.")]
 public class Category
 {
+    /// <summary>
+    /// ID категории.
+    /// </summary>
+    [Comment("ID категории.")]
     public int Id { get; set; }
 
+    /// <summary>
+    /// Название категории.
+    /// </summary>
     [MaxLength(64)]
+    [Comment("Название категории.")]
     public required string Name { get; set; }
-    
+
+    /// <summary>
+    /// Порядок сортировки.
+    /// </summary>
+    [Comment("Порядок сортировки.")]
     public int Order { get; set; }
+
+    /// <summary>
+    /// Определяет, включена ли категория.
+    /// </summary>
+    [Comment("Определяет, включена ли категория.")]
     public bool Enabled { get; set; }
 
-    public ICollection<Phrase>? Phrases { get; set; }
+    /// <summary>
+    /// Фразы, которые входят в эту категорию.
+    /// </summary>
+    public ICollection<Phrase>? Phrases { get; }
 }
