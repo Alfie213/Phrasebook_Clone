65c706dbb971532219c381aa1fa7149eb82466cd
Evgeny Lyashenko <eugene.code@yandex.ru>
2023-09-24 18:14:58 +0300

    Update Setting.cs

--- Database/Entities/Setting.cs
+++ Database/Entities/Setting.cs
@@ -1,11 +1,23 @@
 ﻿namespace Database.Entities;
 
+/// <summary>
+/// Представляет таблицу с настройками.
+/// </summary>
+[Comment("Представляет таблицу с настройками.")]
 public class Setting
 {
+    /// <summary>
+    /// Ключ.
+    /// </summary>
     [Key]
     [MaxLength(128)]
+    [Comment("Ключ.")]
     public required string Key { get; set; }
 
+    /// <summary>
+    /// Значение.
+    /// </summary>
     [MaxLength(32)]
+    [Comment("Значение.")]
     public required string Value { get; set; }
 }
