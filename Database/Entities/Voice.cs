c7c1a309f2a27fd4129f339edfc0a1378392375b
Evgeny Lyashenko <eugene.code@yandex.ru>
2023-09-24 18:24:30 +0300

    Add comments to the Voice table

--- Database/Entities/Voice.cs
+++ Database/Entities/Voice.cs
@@ -2,17 +2,45 @@
 
 namespace Database.Entities;
 
+/// <summary>
+/// Представляет таблицу с голосами.
+/// </summary>
 [Index(nameof(Name), IsUnique = true)]
 public class Voice
+[Comment("Голоса.")]
 {
+    /// <summary>
+    /// ID голоса.
+    /// </summary>
+    [Comment("ID голоса..")]
     public int Id { get; set; }
+
+    /// <summary>
+    /// ID языка, на котором говорит этот голос.
+    /// </summary>
+    [Comment("ID языка, на котором говорит этот голос.")]
     public int LanguageId { get; set; }
 
+    /// <summary>
+    /// Название языка.
+    /// </summary>
     [MaxLength(32)]
+    [Comment("Название языка.")]
     public required string Name { get; set; }
 
+    /// <summary>
+    /// Пол языка.
+    /// </summary>
+    [Comment("Пол языка.")]
     public Gender Gender { get; set; }
 
+    /// <summary>
+    /// Язык, на котором говорит этот голос.
+    /// </summary>
     public Language? Language { get; set; }
-    public ICollection<Audio>? Audios { get; set; }
+
+    /// <summary>
+    /// Аудиофайлы, озвученные этим голосом.
+    /// </summary>
+    public ICollection<Audio>? Audios { get; }
 }
