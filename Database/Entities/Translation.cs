f843d2590ad68b9e3b48098095aad042b001117e
Evgeny Lyashenko <eugene.code@yandex.ru>
2023-09-24 18:20:03 +0300

    Update Translation.cs

--- Database/Entities/Translation.cs
+++ Database/Entities/Translation.cs
@@ -1,15 +1,48 @@
 ﻿namespace Database.Entities;
 
+/// <summary>
+/// Представляет таблицу с переводами.
+/// </summary>
+[Comment("Переводы.")]
 public class Translation
 {
+    /// <summary>
+    /// ID перевода.
+    /// </summary>
+    [Comment("ID перевода.")]
     public int Id { get; set; }
+
+    /// <summary>
+    /// ID переведённой фразы.
+    /// </summary>
+    [Comment("ID переведённой фразы.")]
     public int PhraseId { get; set; }
+
+    /// <summary>
+    /// ID языка, на котором осуществлён перевод.
+    /// </summary>
+    [Comment("ID языка, на котором осуществлён перевод.")]
     public int LanguageId { get; set; }
 
+    /// <summary>
+    /// Текст перевода.
+    /// </summary>
     [MaxLength(128)]
+    [Comment("Текст перевода.")]
     public required string Text { get; set; }
 
-    public Phrase? Phrase { get; set; }
-    public Language? Language { get; set; }
-    public Audio? Audio { get; set; }
+    /// <summary>
+    /// Переведённая фраза.
+    /// </summary>
+    public Phrase? Phrase { get; }
+
+    /// <summary>
+    /// Язык, на котором осуществлён перевод.
+    /// </summary>
+    public Language? Language { get; }
+
+    /// <summary>
+    /// Аудиофайл с переводом.
+    /// </summary>
+    public Audio? Audio { get; }
 }
