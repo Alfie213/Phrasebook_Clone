d01e2f657c085aaaa2c3066f1b888460cb471b70
Evgeny Lyashenko <eugene.code@yandex.ru>
2023-10-06 23:15:21 +0300

    Added Core project

---
+++ Core/Models/UserModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Models;
+
+/// <summary>
+/// Модель для передачи информации о пользователе.
+/// </summary>
+public sealed class UserModel
+{
+    /// <summary>
+    /// Почта.
+    /// </summary>
+    public required string Email { get; set; }
+}
