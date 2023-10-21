6bc4384a916cac853f93dcde83510a5060209612
eugene-coding <eugene.code@yandex.ru>
2023-10-21 12:44:25 +0300

    Fixed registration post request

---
+++ Core/Models/RegistrationModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Core.Models;
+
+/// <summary>
+/// Модель для регистрации пользователя.
+/// </summary>
+[Serializable]
+public sealed class RegistrationModel
+{
+	/// <summary>
+	/// Создаёт экземпляр класса.
+	/// </summary>
+	/// <param name="email">Почта.</param>
+	/// <param name="password">Пароль.</param>
+	[SetsRequiredMembers]
+	public RegistrationModel(string email, string password)
+	{
+		Email = email;
+		Password = password;
+	}
+
+	/// <summary>
+	/// Почта.
+	/// </summary>
+	public required string Email { get; set; }
+
+	/// <summary>
+	/// Пароль.
+	/// </summary>
+	public required string Password { get; set; }
+}
