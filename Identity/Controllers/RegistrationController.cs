49b12561966420d9f0ec926aaccd59061b71b178
eugene-coding <eugene.code@yandex.ru>
2023-10-21 12:44:25 +0300

    Fixed registration post request

--- Identity/Controllers/RegistrationController.cs
+++ Identity/Controllers/RegistrationController.cs
@@ -1,4 +1,6 @@
-﻿using Identity.Extensions;
+﻿using Core.Models;
+
+using Identity.Extensions;
 
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -12,57 +14,46 @@ namespace Identity.Controllers;
 [Route("api/[controller]")]
 public sealed class RegistrationController : ControllerBase
 {
-    private readonly UserManager<IdentityUser> _userManager;
-    private readonly ILogger<RegistrationController> _logger;
+	private readonly UserManager<IdentityUser> _userManager;
+	private readonly ILogger<RegistrationController> _logger;
 
-    /// <summary>
-    /// Создаёт экземпляр класса.
-    /// </summary>
-    /// <param name="userManager"><see cref="UserManager{TUser}"/>.</param>
-    /// <param name="logger">Логгер.</param>
-    public RegistrationController(UserManager<IdentityUser> userManager, ILogger<RegistrationController> logger)
-    {
-        _userManager = userManager;
-        _logger = logger;
-    }
+	/// <summary>
+	/// Создаёт экземпляр класса.
+	/// </summary>
+	/// <param name="userManager"><see cref="UserManager{TUser}"/>.</param>
+	/// <param name="logger">Логгер.</param>
+	public RegistrationController(UserManager<IdentityUser> userManager, ILogger<RegistrationController> logger)
+	{
+		_userManager = userManager;
+		_logger = logger;
+	}
 
-    /// <summary>
-    /// Регистрирует пользователя.
-    /// </summary>
-    /// <param name="email">Почта.</param>
-    /// <param name="password">Пароль.</param>
-    /// <returns>
-    /// <see cref="StatusCodes.Status200OK"/>, если пользователь успешно зарегистрирован,
-    /// иначе - <see cref="StatusCodes.Status400BadRequest"/> с информацией об ошибках.
-    /// </returns>
-    [HttpPost]
-    public async Task<IActionResult> RegisterAsync(string email, string password)
-    {
-        if (string.IsNullOrEmpty(email))
-        {
-            return BadRequest($"{email} не может быть пустым.");
-        }
+	/// <summary>
+	/// Регистрирует пользователя.
+	/// </summary>
+	/// <param name="registrationModel">Модель для регистрации пользователя.</param>
+	/// <returns>
+	/// <see cref="StatusCodes.Status200OK"/>, если пользователь успешно зарегистрирован,
+	/// иначе - <see cref="StatusCodes.Status400BadRequest"/> с информацией об ошибках.
+	/// </returns>
+	[HttpPost]
+	public async Task<IActionResult> RegisterAsync([FromBody] RegistrationModel registrationModel)
+	{
+		var user = new IdentityUser
+		{
+			UserName = registrationModel.Email,
+			Email = registrationModel.Email
+		};
 
-        if (string.IsNullOrEmpty(password))
-        {
-            return BadRequest($"{password} не может быть пустым.");
-        }
+		var result = await _userManager.CreateAsync(user, registrationModel.Password);
 
-        var user = new IdentityUser
-        {
-            UserName = email,
-            Email = email
-        };
+		if (result.Succeeded)
+		{
+			_logger.UserRegistered();
 
-        var result = await _userManager.CreateAsync(user, password);
+			return Ok();
+		}
 
-        if (result.Succeeded)
-        {
-            _logger.UserRegistered();
-
-            return Ok();
-        }
-
-        return BadRequest(result.Errors);
-    }
+		return BadRequest(result.Errors);
+	}
 }
